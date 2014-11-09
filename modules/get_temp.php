1a5ecc8e45d09e851f7b2336f425d1e7a758d98f
Zetaphor <zetaphor@gmail.com>
2014-11-08 20:20:55 -0500

    Updated get_temp to account for wit.ai returning "outside" as a location

--- modules/get_temp.php
+++ modules/get_temp.php
@@ -5,12 +5,21 @@ require 'config.php';
 
 $entities = json_decode($_REQUEST['data'], true);
 
-if (count($entities)) {
-	$weather = getTemp($entities['location'][0]['value']);
-	echo "The temperature in ". $weather['city'] .", ". $weather['state'] ." is ". $weather['temp']. " degrees. The condition is ". $weather['condition'];
-} else {
+function getLocalTemp() {
+	global $default_location;
 	$weather = getTemp($default_location);
-	echo "The temperature is ". $weather['temp'] ." degrees. The condition is ". $weather['condition'];
+	return "The temperature is ". $weather['temp'] ." degrees. The condition is ". $weather['condition'];
 }
+
+function getLocationTemp($location) {
+	$weather = getTemp();
+	return "The temperature in ". $weather['city'] .", ". $weather['state'] ." is ". $weather['temp']. " degrees. The condition is ". $weather['condition'];
+}
+
+if (count($entities)) {
+	// Wit.ai has a problem with realizing that 'outside' is not a location
+	if ($entities['location'][0]['value'] == "outside") echo getLocalTemp();
+	else echo getLocationTemp();
+} else echo getLocalTemp();
 
 ?>
