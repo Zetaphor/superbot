743aeed723ba02b21aa9b3e256f54472dfeee7b8
zetaphor <zetaphor@gmail.com>
2014-11-11 18:24:11 -0500

    Added error checking to rotton tomatoes lib, added get released and get actors

--- modules/lib/rotton_tomatoes.php
+++ modules/lib/rotton_tomatoes.php
@@ -1,31 +1,38 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-require '../kint/Kint.class.php';
-
 $api_key = "u6xsnwvbm4y7tfxy3vtr5tax";
 
-function rottonRequest($query) {
+function rottonRequest($movie_name) {
 	global $api_key;
-	$results = json_decode(file_get_contents('http://api.rottentomatoes.com/api/public/v1.0/movies.json?q='. urlencode($query) .'&page_limit=1&page=1&apikey='. $api_key));
-	$movie_result = $results->movies[0];
-	$movie = array(
-		'poster' => str_replace("_tmb", "_det", $movie_result->posters->profile),
-		'release_year' => $movie_result->year,
-		'synopsis' => $movie_result->synopsis,
-		'audience_rating' => $movie_result->ratings->audience_score,
-		'critic_score' => $movie_result->ratings->critics_score,
-		'cast' => array()
-	);
+	$results = json_decode(file_get_contents('http://api.rottentomatoes.com/api/public/v1.0/movies.json?q='. urlencode($movie_name) .'&page_limit=1&page=1&apikey='. $api_key));
+	if (count($results->movies)) {
+		$movie_result = $results->movies[0];
+		$movie = array(
+			'poster' => str_replace("_tmb", "_det", $movie_result->posters->profile),
+			'release_year' => $movie_result->year,
+			'synopsis' => $movie_result->synopsis,
+			'audience_rating' => $movie_result->ratings->audience_score,
+			'critic_score' => $movie_result->ratings->critics_score,
+			'cast' => array()
+		);
 
-	foreach($movie_result->abridged_cast as $actor) {
-		$movie['cast'][] = $actor->name;
-	}
+		foreach($movie_result->abridged_cast as $actor) {
+			$movie['cast'][] = $actor->name;
+		}
 
-	return $movie;
+		return $movie;
+	} else return false;
 }
 
-rottonRequest('Beyond the lights');
+function getRelease($movie_name) {
+	$movie = rottonRequest($movie_name);
+	if ($movie) return $movie['release_year'];
+	else return false;
+}
+
+function getCast($movie_name) {
+	$movie = rottonRequest($movie_name);
+	if ($movie) return $movie['cast'];
+	else return false;
+}
 
 ?>
--- modules/movie_search.php
+++ modules/movie_search.php
@@ -1,6 +1,24 @@
-<?
-
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 require 'kint/Kint.class.php';
 require 'lib/rotton_tomatoes.php';
+$entities = json_decode($_REQUEST['data'], true);
+
+$find_actors = false;
+$find_release = false;
+$movie_title = $entities['movie_title'][0]['value'];
+
+if (isset($entities['movie_release_date'])) {
+	$release = getRelease($movie_title);
+	if ($release) echo ucwords($movie_title)  .' was released in '. $release;
+	else echo 'Sorry, I was unable to find a movie called '. ucwords($movie_title);
+} elseif (isset($entities['movie_actors'])) {
+	$actors = getCast($movie_title);
+	if ($actors) {
+		$actors[(count($actors) -1)] = ' and '. $actors[(count($actors) - 1)];
+		echo ucwords($movie_title) ." starred ". implode(', ', $actors);
+	} else echo 'Sorry, I was unable to find a movie called '. ucwords($movie_title);
+}
 
 ?>
