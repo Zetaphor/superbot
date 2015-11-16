b40b71d1b8b99afd91e4d15f2c00f194f04a0504
Gary Daigle <zetaphor@gmail.com>
2015-11-16 15:18:12 -0500

    Fixed weather module not seeing default location

--- modules/get_temp.php
+++ modules/get_temp.php
@@ -1,4 +1,5 @@
 <?php
+require 'config.php';
 require 'lib/yql_weather.php';
 $entities = [];
 if (isset($_REQUEST['entities'])) $entities = json_decode($_REQUEST['data'], true);
@@ -6,12 +7,15 @@ if (isset($_REQUEST['entities'])) $entities = json_decode($_REQUEST['data'], tru
 function getLocalTemp() {
 	global $default_location;
 	$weather = getTemp($default_location);
-	return "The temperature is ". $weather['temp'] ." degrees. The condition is ". $weather['condition'];
+    if (count($weather)) return "The temperature is ". $weather['temp'] ." degrees. The condition is ". $weather['condition'];
+    else return false;
 }
 
 function getLocationTemp($location) {
 	$weather = getTemp($location);
-	return "The temperature in ". $weather['city'] .", ". $weather['state'] ." is ". $weather['temp']. " degrees. The condition is ". $weather['condition'];
+    if (count($weather)) {
+        return "The temperature in ". $weather['city'] .", ". $weather['state'] ." is ". $weather['temp']. " degrees. The condition is ". $weather['condition'];
+    } else return false;
 }
 
 $response = [
@@ -20,10 +24,17 @@ $response = [
     'html' => ''
 ];
 
+$weather_response = '';
 if (count($entities)) {
 	// Wit.ai has a problem with realizing that 'outside' is not a location
-	if ($entities['location'][0]['value'] == "outside") $response['message'] = getLocalTemp();
-	else $response['message'] = getLocationTemp($entities['location'][0]['value']);
-} else $response['message'] = getLocalTemp();
+	if ($entities['location'][0]['value'] == "outside") $weather_response = getLocalTemp();
+	else $weather_response = getLocationTemp($entities['location'][0]['value']);
+} else $weather_response = getLocalTemp();
+
+if (!$weather_response) {
+    $response['success'] = false;
+    $response['message'] = 'Sorry, there was a problem connecting to the weather service';
+} else $response['message'] = $weather_response;
+
 
 echo json_encode($response);
